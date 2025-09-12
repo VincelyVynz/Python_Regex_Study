34eba15a26a08636b788ac00d1d61cb3098d59bd
Vynz <76029153+VincelyVynz@users.noreply.github.com>
2025-09-12 17:34:53 +0530

    removed unwanted ^ in username_validator.py

--- username_validator.py
+++ username_validator.py
@@ -3,7 +3,7 @@ import re
 #Username Validation (only lowercase letters with a length of 3-5)
 username = input("Enter your name: ")
 
-if re.fullmatch(r"^[a-z][\w]{2,4}", username):
+if re.fullmatch(r"[a-z][\w]{2,4}", username):
     print("Your name is valid")
 else:
     print("Your name is not valid")
