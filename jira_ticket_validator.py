25ad512fb909fc645724fb66bdf4c8f52d053322
Vynz <76029153+VincelyVynz@users.noreply.github.com>
2025-09-12 18:09:14 +0530

    add validation function to jira_ticket_validator.py

--- jira_ticket_validator.py
+++ jira_ticket_validator.py
@@ -1,6 +1,14 @@
+import re
 #Jira Ticket Validator
 
 # Validation conditions (Format: PROJECT-123)
 # Project part = 2–5 uppercase letters
 # Dash (-)
 # Number part = at least 1 digit
+
+def validate_jira_ticket(ticket):
+
+    if re.fullmatch(r"[A-Z]{2,5}-[0-9]{1,}", ticket):
+        return f"{ticket} is valid"
+    else:
+        return f"{ticket} is not valid"
