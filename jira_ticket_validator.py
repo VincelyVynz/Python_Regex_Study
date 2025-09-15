5804005ff36348798342b5f3a9d62c7c8394047d
Vynz <76029153+VincelyVynz@users.noreply.github.com>
2025-09-15 15:51:02 +0530

    Rewrite jira_ticket_validator.py using regex

--- jira_ticket_validator.py
+++ jira_ticket_validator.py
@@ -1,40 +1,56 @@
 import re
 #Jira Ticket Validator
-
 # Validation conditions (Format: PROJECT-123)
-# Project part = 2–5 uppercase letters
-# Dash (-)
-# Number part = 1-4 digits
+# Project part = 2–5 uppercase letters, Dash (-), Number part = 1-4 digits
 
 def validate_jira_ticket(ticket):
-    hyphen_count = 0
-    error_list = []
-    for char in ticket:
-        if char == "-":
-            hyphen_count += 1
-    if hyphen_count == 0:
-        error_list.append("Must contain one - character.")
-    elif hyphen_count > 1:
-        error_list.append("Must not contain more than one - character.")
+    if ticket.count("-") != 1:
+        return "Ticket must contain exactly one hyphen."
     else:
         project, issue = ticket.split("-", 1)
-
         if not re.fullmatch(r"[A-Z]{2,5}", project):
-            error_list.append("Jira project name must be 2 -5 uppercase letters.")
+            return "Jira project name must contain 2-5 uppercase letters."
 
         if not re.fullmatch(r"[0-9]{1,4}", issue):
-            error_list.append("Jira issue name must be 1 - 4 digits.")
+            return "Jira issue must contain 1-4 digits."
 
+    return f"{ticket} is valid."
 
-    if error_list:
-        return f"{ticket}\n" + "\n".join(f"{error}" for error in error_list)
+    # -------- Old validation -------- #
+    # hyphen_count = 0
+    # error_list = []
+    # for char in ticket:
+    #     if char == "-":
+    #         hyphen_count += 1
+    # if hyphen_count == 0:
+    #     error_list.append("Must contain one - character.")
+    # elif hyphen_count > 1:
+    #     error_list.append("Must not contain more than one - character.")
+    # else:
+    #     project, issue = ticket.split("-", 1)
+    #
+    #     if not re.fullmatch(r"[A-Z]{2,5}", project):
+    #         error_list.append("Jira project name must be 2 -5 uppercase letters.")
+    #
+    #     if not re.fullmatch(r"[0-9]{1,4}", issue):
+    #         error_list.append("Jira issue name must be 1 - 4 digits.")
+    #
+    #
+    # if error_list:
+    #     return f"{ticket}\n" + "\n".join(f"{error}" for error in error_list)
+    #
+    # else:
+    #     return f"{ticket} is valid."
 
-    else:
-        return f"{ticket} is valid."
-
-
-# Old validation
+# -------- Simple regex validation -------- #
 # if re.fullmatch(r"[A-Z]{2,5}-[0-9]{1,}", ticket):
 #     return f"{ticket} is valid."
 # else:
 #     return f"{ticket} is not valid."
+
+
+
+
+
+
+
