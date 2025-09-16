3180078e82fd37b01585d00d7a25250cac0766cc
Vynz <76029153+VincelyVynz@users.noreply.github.com>
2025-09-16 17:22:13 +0530

    Clean up jira_ticket_validator.py

--- jira_ticket_validator.py
+++ jira_ticket_validator.py
@@ -16,37 +16,6 @@ def validate_jira_ticket(ticket):
 
     return f"{ticket} is valid."
 
-    # -------- Old validation -------- #
-    # hyphen_count = 0
-    # error_list = []
-    # for char in ticket:
-    #     if char == "-":
-    #         hyphen_count += 1
-    # if hyphen_count == 0:
-    #     error_list.append("Must contain one - character.")
-    # elif hyphen_count > 1:
-    #     error_list.append("Must not contain more than one - character.")
-    # else:
-    #     project, issue = ticket.split("-", 1)
-    #
-    #     if not re.fullmatch(r"[A-Z]{2,5}", project):
-    #         error_list.append("Jira project name must be 2 -5 uppercase letters.")
-    #
-    #     if not re.fullmatch(r"[0-9]{1,4}", issue):
-    #         error_list.append("Jira issue name must be 1 - 4 digits.")
-    #
-    #
-    # if error_list:
-    #     return f"{ticket}\n" + "\n".join(f"{error}" for error in error_list)
-    #
-    # else:
-    #     return f"{ticket} is valid."
-
-# -------- Simple regex validation -------- #
-# if re.fullmatch(r"[A-Z]{2,5}-[0-9]{1,}", ticket):
-#     return f"{ticket} is valid."
-# else:
-#     return f"{ticket} is not valid."
 
 
 
