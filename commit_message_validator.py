c6b51b3424e4519c75139713e1742da486339806
Vynz <76029153+VincelyVynz@users.noreply.github.com>
2025-09-16 17:21:08 +0530

    Clean up commit_message_validator.py

--- commit_message_validator.py
+++ commit_message_validator.py
@@ -1,6 +1,5 @@
 import re
-from jira_ticket_validator import validate_jira_ticket
-#Commit message validator
+# -------------------Commit message validator ------------------- #
 # Format: <type>(<scope>): <subject> [JIRA-ID]
 # Example: feature(auth): add login flow HSU-123
 
@@ -33,42 +32,12 @@ def commit_validator(message):
                 print(f"✅{name}:{value}")
         if fully_valid:
             print(f"{message} is a valid commit message.")
+        else:
+            print(f"{message} is not a valid commit message.")
     else:
         print("Missing or invalid format.")
 
 
-#commit_validator("feature(auth): [HSU-123] add login flow ")
-commit_validator("feature(auth): add login flow [HSU-123] ")
 
-# types_list = ["feat", "feature", "fix", "bug", "docs", "style", "refactor", "perf", "test", "build", "ci", "chore", "revert"]
-# if message.count(":") != 1:
-#     return "Commit message must contain exactly one colon."
-#
-# else:
-#     type_scope, subject_jira_id = message.split(":",1)
-#     type_scope, subject_jira_id = type_scope.strip(), subject_jira_id.strip()
-#
-#     if not ("(" in type_scope and type_scope.endswith(")")):
-#         return "Invalid type scope."
-#     else:
-#         type_part,scope = type_scope.split("(",1)
-#         scope = scope[:-1]
-#         if not scope:
-#             return "Missing scope."
-#
-#         if type_part not in types_list:
-#             return "Invalid type."
-#
-#     if " " not in subject_jira_id:
-#         return "Missing subject or jira ID."
-#     else:
-#         subject, jira_id = subject_jira_id.rsplit(" ",1)
-#
-#         jira_result = validate_jira_ticket(jira_id)
-#         if  not jira_result.endswith("is valid."):
-#             return jira_result
-#
-#
-#     return f"Type: {type_part}\nScope: {scope}\nSubject: {subject}\nJira ID: {jira_id}\n{message} is a valid commit message"
 
 
