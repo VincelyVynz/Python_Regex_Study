9e7fefe9b17f7f1dc5804b15c5830dce1bb1ac85
Vynz <76029153+VincelyVynz@users.noreply.github.com>
2025-09-16 17:14:16 +0530

    Fixing for loop in commit_message_validator.py using regex

--- commit_message_validator.py
+++ commit_message_validator.py
@@ -6,35 +6,39 @@ from jira_ticket_validator import validate_jira_ticket
 
 def commit_validator(message):
     message = message.strip()
-    parts = re.match(r"^(?P<type_part>feat|feature|fix|bug|docs|style|refactor|perf|test|build|ci|chore|revert)"
-                     r"\((?P<scope>[^)]+)\):\s*"
+
+    parts = re.match(r"^(?P<type_part>feat|feature|fix|bug|docs|style|refactor|perf|test|build|ci|chore|revert)?"
+                     r"\((?P<scope>[^)]+)?\):\s*"
                      r"(?:"
-                     r"(?P<jira_id1>\[[A-Z]{2,5}-[0-9]{1,4}\])\s*(?P<subject1>.+)"
+                     r"(?P<jira_id1>\[[A-Z]{2,5}-[0-9]{1,4}\])\s*(?P<subject1>.+)?"
                      r"|"
-                     r"(?P<subject2>.+?)\s*(?P<jira_id2>\[[A-Z]{2,5}-[0-9]{1,4}\])"
+                     r"(?P<subject2>.+?)\s*(?P<jira_id2>\[[A-Z]{2,5}-[0-9]{1,4}\])?"
                      r")$", message)
 
 
     if parts:
+
         result = {
         "type_part" : parts.group("type_part"),
         "scope" : parts.group("scope"),
         "subject" : parts.group("subject1") or parts.group("subject2"),
         "jira_id" : parts.group("jira_id1") or parts.group("jira_id2")
         }
-
+        fully_valid = True
         for name, value in result.items():
             if value is None:
-                result[name] = "Missing or invalid."
+                print(f"❌{[name]} is missing or invalid.")
+                fully_valid = False
             else:
-                result[name] = value
-            print(result)
+                print(f"✅{name}:{value}")
+        if fully_valid:
+            print(f"{message} is a valid commit message.")
     else:
         print("Missing or invalid format.")
 
 
-# commit_validator("feature(auth): [HSU-123] add login flow ")
-commit_validator("feature(auth): add login flow HSU-123 ")
+#commit_validator("feature(auth): [HSU-123] add login flow ")
+commit_validator("feature(auth): add login flow [HSU-123] ")
 
 # types_list = ["feat", "feature", "fix", "bug", "docs", "style", "refactor", "perf", "test", "build", "ci", "chore", "revert"]
 # if message.count(":") != 1:
