3e6b849e02e1c83140236460e8e91002472c69b5
Vynz <76029153+VincelyVynz@users.noreply.github.com>
2025-09-17 17:44:02 +0530

    Add groupdict method and header paramater to custom_url_validator.py

--- custom_url_validator.py
+++ custom_url_validator.py
@@ -5,16 +5,16 @@ import re
 # font = ('Arial', 24, 'italic') normal, bold
 # #000000 black #FFFFFF white
 def custom_image(url):
-    if re.match(r"^(?P<length>length=\d{2,4})/(?P<width>width=\d{2,4})/"
+    parts= re.match(r"^(?P<length>length=\d{2,4})/(?P<width>width=\d{2,4})/"
                 r"(?P<bg_color>bg_color=#[A-Z0-9]{6})/(?P<fg_color>fg_color=#[A-Z0-9]{6})/"
                 r"(?P<border>border=y|n)/(?P<orientation>orientation=v|h)/"
-                r"(?P<font>font=\([A-Za-z\s]+,[\d]{1,2},[A-Za-z\s]+\))/(?P<text>text=.*)$",
-                url):
-        print("Valid URL")
+                r"(?P<font>font=\([A-Za-z\s]+,[\d]{1,2},[A-Za-z\s]+\))/(?P<header>header=.*?)?(?P<text>text=.*)$",
+                url)
+
+    if parts:
+        print(parts.groupdict())
     else:
-        print("Invalid URL")
-
-
+        print(f"Invalid url")
 custom_image("length=90/width=45/bg_color=#FFFFFF/fg_color=#000000/border=y/orientation=v/font=(Arial,24,italic)/text=message")
 
 
